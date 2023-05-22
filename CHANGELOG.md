b58a4ffbc05f0a255a512d8856be1f87cdf64f2b
Mark Battistella <markbattistella@users.noreply.github.com>
2023-05-22 14:30:30 +1000

    v5.0.6 - cleanup to code

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+
+## 5.0.6 - Mon May 22 2023
+
+### Fixed
+
+- Published to incorrect package manager
+
+## 5.0.5 - Mon May 22 2023
+
+### Fixed
+
+- Namespace issue - wrapped the script into its own local environment to avoid clashing
+- Addition into Docsify.js plugin list more inline with the documentation
--- docs/dist/docsify-sidebar.js
+++ docs/dist/docsify-sidebar.js
@@ -1,238 +1,184 @@
-/*! docsify-sidebar.js v5.0.4 | (c) Mark Battistella */
-'use strict';
+/*! docsify-sidebar 5.0.6 | (c) Mark Battistella */
+; (function () {
+	'use strict';
 
-// MARK: - check if object exists and is not empty
-function doesObjectExists(obj) {
-	return (
-		obj !== undefined &&
-		obj !== null &&
-		obj.constructor === Object &&
-		Object.keys(obj).length > 0
-	);
-}
+	const autoFooter = (hook, vm) => {
+		// Default options
+		const footerOptions = {
+			name: '',
+			url: '',
+			copyYear: '',
+			policy: true,
+			terms: true,
+			cookies: true,
+			customStyle: 'body'
+		};
 
+		// Extend defaults with user options
+		Object.assign(footerOptions, $docsify.autoFooter || {});
 
-// MARK: - update the `options` object
-function getFooter(footerOptions) {
+		const isObject = (obj) => obj && obj.constructor === Object && Object.keys(obj).length > 0;
 
-	// -- get this year
-	let date = new Date().getFullYear();
-
-	// -- update the variables
-	footerOptions.name ? footerOptions.name : null;
-	footerOptions.url ? footerOptions.url : null;
-	footerOptions.copyYear ? footerOptions.copyYear : date;
-	footerOptions.policy ? footerOptions.policy : false;
-	footerOptions.terms ? footerOptions.terms : false;
-	footerOptions.cookies ? footerOptions.cookies : false;
-	footerOptions.customStyle ? footerOptions.customStyle : false;
-}
-
-
-// -- defaults
-const footerOptions = {
-	name:        '',
-	url:         '',
-	copyYear:    '',
-	policy:      true,
-	terms:       true,
-	cookies:     true,
-	customStyle: false
-},
-
-// -- errors
-footerError = {
-
-	// -- error: when something that shouldn't happen, happens
-	unknownError: 'ERROR: sidebar-footer plugin - unknown error',
-
-    // -- error: configuration not set
-    configNotSet: `ERROR: sidebar-footer plugin - configuration not set\n\t\t>> this happens when the autoFooter is not found in the index.html file`,
-
-    // -- error: configuration is empty object
-    configIsEmpty: `ERROR: sidebar-footer plugin - configuration is empty\n\t\t>> please consult the documentation for the settings needed`
-};
-
-
-// -- run the function
-function autoFooter(hook, vm) {
-
-	// MARK: run with docsify init
-	hook.init(function () {
-
-		// -- initialise the options
-		getFooter(footerOptions);
-
-
-		// -- check the options for bool or string
-		if (typeof footerOptions.customStyle === "boolean" || typeof footerOptions.customStyle === "string") {
-
-			// -- dont continue if using custom styles
-			if ((typeof footerOptions.customStyle === "boolean" && footerOptions.customStyle === true)) {
-				return;
-			}
-
-			// -- global style
-			let style = `#mb-footer { border-top: 1px solid; font-size: 0.8em; line-height: 1.5; transition: all var(--sidebar-transition-duration) ease-out; }`;
-
-			// -- custom style for sidebar
-			if ((typeof footerOptions.customStyle === "boolean" && footerOptions.customStyle === false) ||
-				(footerOptions.customStyle === "sidebar")
-			) {
-				style += `#mb-footer { padding-top: 1.5rem; margin-top: 1.5rem; } #mb-footer .footer-text, #mb-footer .footer-text a { font-weight: bold; }`;
-			}
-
-			// -- custom style for sidebar
-			if (footerOptions.customStyle === "body") {
-				
-				// --> if there is a sidebar
-				if( $docsify.loadSidebar || $docsify.loadSidebar === null || !$docsify.hideSidebar ) {
-					style += `body #mb-footer { margin-left: var(--sidebar-width); } body.close #mb-footer { margin-left: 0; }`;
-				}
-
-				// --> standard
-				style += `#mb-footer { padding: 1.5rem; } #mb-footer .footer-container { max-width: var(--content-max-width); margin: 0 auto; } #mb-footer .footer-container { display: grid; grid-template-columns: auto auto; } #mb-footer .footer-container a { margin-left: 2em; } #mb-footer .footer-link { text-align: right; }`;
-
-				// --> media queries
-				style += `@media (max-width: 680px) { #mb-footer .footer-container { grid-template-columns: auto; }#mb-footer .footer-text, #mb-footer .footer-link { text-align: center; } } @media (max-width: 400px) { #mb-footer .footer-text, #mb-footer .footer-link { text-align: left; } #mb-footer span { display: block; } #mb-footer .footer-container a { margin: 0; } }`;
-			}
-
-			// create the variables
-			const head = document.querySelector("head"),
-				  sheet = document.createElement("style");
-
-			// add to the page
-			head.appendChild(sheet);
-			sheet.appendChild(document.createTextNode(style));
+		if (!isObject(footerOptions)) {
+			throw new Error('Sidebar-footer configuration is invalid or empty. Please check the $docsify.autoFooter config.');
 		}
-	});
 
+		// Auto-update the year
+		if (!footerOptions.copyYear) {
+			footerOptions.copyYear = new Date().getFullYear();
+		}
 
-	// MARK: after the HTML appended to DOM
-	hook.doneEach(function () {
+		// MARK: run with docsify init
+		hook.init(function () {
 
-		// set the scope
-		const contentScope = document.getElementById("mb-footer");
+			// -- initialise the options
+			// getFooter(footerOptions, $docsify.autoFooter || {});
 
-		// if the scope is empty
-		if (!contentScope) { return; }
+			// -- check the options for bool or string
+			if (typeof footerOptions.customStyle === "boolean" || typeof footerOptions.customStyle === "string") {
 
-		//
-		// MARK: - add the info
-		//
+				// -- dont continue if using custom styles
+				if ((typeof footerOptions.customStyle === "boolean" && footerOptions.customStyle === true)) {
+					return;
+				}
 
-		// get the date
-		const date = new Date().getFullYear(),
+				// -- global style
+				let style = `#mb-footer { border-top: 1px solid; font-size: 0.8em; line-height: 1.5; transition: all var(--sidebar-transition-duration) ease-out; }`;
 
-			// -- url building
-			baseUrl = window.location.origin + window.location.pathname + "#/",
+				// -- custom style for sidebar
+				if ((typeof footerOptions.customStyle === "boolean" && footerOptions.customStyle === false) ||
+					(footerOptions.customStyle === "sidebar")
+				) {
+					style += `#mb-footer { padding-top: 1.5rem; margin-top: 1.5rem; } #mb-footer .footer-text, #mb-footer .footer-text a { font-weight: bold; }`;
+				}
 
-			// -- check if link is internal or external
-			isExternalLink = (url) => {
-				const tmp = document.createElement('a');
-				tmp.href = url;
-				return tmp.host !== window.location.host;
-			},
+				// -- custom style for sidebar
+				if (footerOptions.customStyle === "body") {
 
-			// -- link generator
-			createLink = (option, linkText, defaultLink, className) => {
-				let result = "";
-
-				// --> only accept bool and string
-				if (typeof option === "boolean" || typeof option === "string") {
-
-					// --> if bool, and true
-					if (typeof option === "boolean" && option) {
-
-						// --> use the default options
-						result = `<a href="${baseUrl + defaultLink}">${linkText}</a>`;
-
-						// --> if it is a string url
-					} else if (typeof option === "string") {
-
-						// --> is the link external
-						result = isExternalLink(option)
-
-							// --> if external, add the _blank
-							? `<a target="_blank" href="${option}">${linkText}</a>`
-
-							// --> if internal, then add the page name
-							: `<a href="${baseUrl + option}">${linkText}</a>`;
+					// --> if there is a sidebar
+					if ($docsify.loadSidebar || $docsify.loadSidebar === null || !$docsify.hideSidebar) {
+						style += `body #mb-footer { margin-left: var(--sidebar-width); } body.close #mb-footer { margin-left: 0; }`;
 					}
+
+					// --> standard
+					style += `#mb-footer { padding: 1.5rem; } #mb-footer .footer-container { max-width: var(--content-max-width); margin: 0 auto; } #mb-footer .footer-container { display: grid; grid-template-columns: auto auto; } #mb-footer .footer-container a { margin-left: 2em; } #mb-footer .footer-link { text-align: right; }`;
+
+					// --> media queries
+					style += `@media (max-width: 680px) { #mb-footer .footer-container { grid-template-columns: auto; }#mb-footer .footer-text, #mb-footer .footer-link { text-align: center; } } @media (max-width: 400px) { #mb-footer .footer-text, #mb-footer .footer-link { text-align: left; } #mb-footer span { display: block; } #mb-footer .footer-container a { margin: 0; } }`;
 				}
 
-				// --> if the result is not empty
-				if (result) {
+				// create the variables
+				const head = document.querySelector("head"),
+					sheet = document.createElement("style");
 
-					// --> create the class name
-					const classname = `${className.toLowerCase().replace(/\s+/g, '-')}`;
+				// add to the page
+				head.appendChild(sheet);
+				sheet.appendChild(document.createTextNode(style));
+			}
+		});
 
-					// -- compile the elements
-					result = `<span class="${classname}">${result}</span>`;
-				}
+		// MARK: after the HTML appended to DOM
+		hook.doneEach(function () {
 
-				return result;
-			},
+			// set the scope
+			const contentScope = document.getElementById("mb-footer");
 
-			// MARK: - html elements
+			// if the scope is empty
+			if (!contentScope) { return; }
 
-			divclose = `</div>`,
+			//
+			// MARK: - add the info
+			//
 
-			// -- divs
-			div1open = `<div class="footer-container">`,
-			div2open = `<div class="footer-text">`,
-			div3open = `<div class="footer-link">`,
+			// get the date
+			const date = new Date().getFullYear(),
 
-			// -- text
-			copyright = (
-				`<span class="footer-text-copyright">Copyright &copy; ${
-					footerOptions.copyYear && footerOptions.copyYear <= date
+				// -- url building
+				baseUrl = window.location.origin + window.location.pathname + "#/",
+
+				// -- check if link is internal or external
+				isExternalLink = (url) => {
+					const tmp = document.createElement('a');
+					tmp.href = url;
+					return tmp.host !== window.location.host;
+				},
+
+				// -- link generator
+				createLink = (option, linkText, defaultLink, className) => {
+					let result = "";
+
+					// --> only accept bool and string
+					if (typeof option === "boolean" || typeof option === "string") {
+
+						// --> if bool, and true
+						if (typeof option === "boolean" && option) {
+
+							// --> use the default options
+							result = `<a href="${baseUrl + defaultLink}">${linkText}</a>`;
+
+							// --> if it is a string url
+						} else if (typeof option === "string") {
+
+							// --> is the link external
+							result = isExternalLink(option)
+
+								// --> if external, add the _blank
+								? `<a target="_blank" href="${option}">${linkText}</a>`
+
+								// --> if internal, then add the page name
+								: `<a href="${baseUrl + option}">${linkText}</a>`;
+						}
+					}
+
+					// --> if the result is not empty
+					if (result) {
+
+						// --> create the class name
+						const classname = `${className.toLowerCase().replace(/\s+/g, '-')}`;
+
+						// -- compile the elements
+						result = `<span class="${classname}">${result}</span>`;
+					}
+
+					return result;
+				},
+
+				// MARK: - html elements
+
+				divclose = `</div>`,
+
+				// -- divs
+				div1open = `<div class="footer-container">`,
+				div2open = `<div class="footer-text">`,
+				div3open = `<div class="footer-link">`,
+
+				// -- text
+				copyright = (
+					`<span class="footer-text-copyright">Copyright &copy; ${footerOptions.copyYear && footerOptions.copyYear <= date
 						? `${footerOptions.copyYear}${footerOptions.copyYear < date ? "&#45;" + date : ""}`
 						: date
-				}</span>`
-			),
-			author = createLink( footerOptions.url, footerOptions.name, '', 'footer-text-author'),
+					}</span>`
+				),
+				author = createLink(footerOptions.url, footerOptions.name, '', 'footer-text-author'),
 
-			// -- links
-			policyURL = createLink(footerOptions.policy, 'Policy', '_policy', 'footer-links-policy'),
-			termsURL = createLink(footerOptions.terms, 'Terms', '_terms', 'footer-links-terms' ),
-			cookiesURL = createLink(footerOptions.cookies, 'Cookies', '_cookies', 'footer-links-cookies'),
+				// -- links
+				policyURL = createLink(footerOptions.policy, 'Policy', '_policy', 'footer-links-policy'),
+				termsURL = createLink(footerOptions.terms, 'Terms', '_terms', 'footer-links-terms'),
+				cookiesURL = createLink(footerOptions.cookies, 'Cookies', '_cookies', 'footer-links-cookies'),
 
-			// output
-			output = (
-				div1open +
-					div2open + copyright + author +	divclose +
+				// output
+				output = (
+					div1open +
+					div2open + copyright + author + divclose +
 					div3open + policyURL + termsURL + cookiesURL + divclose +
-				divclose
-			);
+					divclose
+				);
 
-		contentScope.innerHTML = output;
-	});
-}
+			contentScope.innerHTML = output;
+		});
+	}
 
-
-// MARK: - check options is defined and not empty
-if (typeof footerOptions !== 'undefined' && doesObjectExists(footerOptions)) {
-
-	// -- find footer plugin options
-	window.$docsify.autoFooter = Object.assign(
-		footerOptions,
-		window.$docsify.autoFooter
-	);
-	window.$docsify.plugins = [].concat(autoFooter, window.$docsify.plugins);
-
-} else {
-
-    // --> config not set in `index.html`
-    if (typeof headerOptions === 'undefined') {
-        throw footerError.configNotSet
-    }
-
-    // --> config is empty object
-    if (!doesObjectExists(headerOptions)) {
-        throw footerError.configNotSet
-    }
-
-    // --> some other error
-    throw footerError.unknownError;
-}
+	// Register the plugin
+	$docsify = $docsify || {};
+	$docsify.plugins = [].concat(autoFooter, $docsify.plugins || []);
+}());
